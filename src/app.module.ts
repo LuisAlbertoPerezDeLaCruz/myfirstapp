c42a25c5f87898e028f8477f530ef5d7a170ca6d
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-09-06 18:51:42 -0400

    creado payments con resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { HelloModule } from './hello/hello.module';
 import { UsersModule } from './users/users.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [TasksModule, HelloModule, UsersModule],
+  imports: [TasksModule, HelloModule, UsersModule, PaymentsModule],
   controllers: [],
   providers: [],
 })
