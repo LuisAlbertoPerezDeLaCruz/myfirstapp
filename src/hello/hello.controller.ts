12ce1c91982a1ce2e1fe13bd20098e201fdc75d9
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-09-06 09:51:16 -0400

    avance

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
+import { ValidateuserPipe } from './pipes/validateuser/validateuser.pipe';
 
 @Controller()
 export class HelloController {
@@ -10,4 +20,23 @@ export class HelloController {
       message: 'Hello World',
     });
   }
+
+  @Get('hello/ticket/:id')
+  async getTicket(@Param('id', ParseIntPipe) id: number) {
+    console.log(typeof id);
+  }
+
+  @Get('hello/ticket-status/:active')
+  async getStatus(@Param('active', ParseBoolPipe) active: boolean) {
+    console.log(typeof active);
+  }
+
+  // @Get('hello/greets')
+  // greets(@Query() query: { name: string; age: number }) {
+  //   console.log('name', query.name);
+  //   console.log('age', query.age);
+  // }
+
+  @Get('hello/greets')
+  greets(@Query(ValidateuserPipe) query: { name: string; age: number }) {}
 }
---
+++ src/hello/pipes/validateuser/validateuser.pipe.ts
@@ -0,0 +1,21 @@
+import {
+  ArgumentMetadata,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+
+@Injectable()
+export class ValidateuserPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    console.log('value:', value);
+    const ageNumber = parseInt(value.age);
+    if (isNaN(ageNumber)) {
+      throw new HttpException('Age must be a number', HttpStatus.BAD_REQUEST);
+    }
+    value = { ...value, age: ageNumber };
+    return value;
+  }
+}
